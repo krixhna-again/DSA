414977ecdc21b427d0d6935a3b9c625f5a645450
Krishna <krishna0017k@gmail.com>
2026-08-29 15:27:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Alternates in an Array/alternates-in-an-array.js
@@ -0,0 +1,11 @@
+class Solution {
+    getAlternates(arr) {
+        let result = [];
+
+        for (let i = 0; i < arr.length; i += 2) {
+            result.push(arr[i]);
+        }
+
+        return result;
+    }
+}
